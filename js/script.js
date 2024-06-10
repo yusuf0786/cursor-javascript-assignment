5c901b2aa65e498deb3e2fd6298a48862405b5b7
Yusuf Ansari <yusssuf0786@gmail.com>
2024-06-10 17:16:39 +0530

    speed reduced & direction of the element changes to mouse clicked

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const movingBox = document.querySelector(".moving-element");
 document.addEventListener("click", (event) => {
   const x = event.clientX;
   const y = event.clientY;
+  const elementCurrentX = movingBox.offsetLeft + movingBox.offsetWidth / 2
   movingBox.style.left = `${x}px`;
   movingBox.style.top = `${y}px`;
+  movingBox.style.transform = elementCurrentX <= x ? `scaleX(-1)` : ``;
+
+  
 });
